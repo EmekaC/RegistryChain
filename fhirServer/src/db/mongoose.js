ceadb4fd9354adbe8adc01cff760cd82e13cb430
EmekaC <emeka2015@gmail.com>
2021-11-25 16:43:28 +0100

    Update mongoose.js
    Adding comments

--- fhirServer/src/db/mongoose.js
+++ fhirServer/src/db/mongoose.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
+//Connecting to mongodb
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
