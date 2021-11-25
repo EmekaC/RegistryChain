c14726bd997168efbb382670ab2d59eede8c9c29
EmekaC <emeka2015@gmail.com>
2021-11-25 16:41:17 +0100

    Update patientAuth.js
    Just added comments

--- fhirServer/src/middleware/patientAuth.js
+++ fhirServer/src/middleware/patientAuth.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const Patient = require('../models/patient')
 
 
+//Verification of JWT to give access to a patient to other parts of their account and perform operations
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'thisisapatient')
+        const decoded = jwt.verify(token, process.env.PATIENT_JWT_SECRET)
         const patient = await Patient.findOne({ _id: decoded._id, 'tokens.token': token })
         
         if (!patient) {
