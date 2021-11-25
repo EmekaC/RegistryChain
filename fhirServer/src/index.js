adddd1afb2f283d66f8aaba84c509d56412573ee
EmekaC <emeka2015@gmail.com>
2021-11-25 16:44:23 +0100

    Update index.js
    Added the route to Temp.js

--- fhirServer/src/index.js
+++ fhirServer/src/index.js
@@ -1,19 +1,23 @@
-const express = require('express')
 require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
 require('./db/mongoose')
 const practRouter = require('./routers/practitioner')
 const patientRouter = require('./routers/patient')
 const phrRouter = require('./routers/phr')
-
+const tempRouter = require('./routers/temp')
 
 const app = express()
 const port = process.env.PORT
 
 
+app.use(cors());
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(practRouter)
 app.use(patientRouter)
 app.use(phrRouter)
+app.use(tempRouter)
 
 
 app.listen(port, () => {
