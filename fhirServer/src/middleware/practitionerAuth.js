3aa966ed18749e7840e1c56055e49445afee6f5f
EmekaC <emeka2015@gmail.com>
2021-11-25 16:40:45 +0100

    Update practitionerAuth.js
    Just added comments

--- fhirServer/src/middleware/practitionerAuth.js
+++ fhirServer/src/middleware/practitionerAuth.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken')
 const Practitioner = require('../models/practitioner')
 
 
+//Verification of JWT to give access to a practitioner to other parts of their account and perform operations
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, 'thisisapractitioner')
+        const decoded = jwt.verify(token, process.env.PRACTITIONER_JWT_SECRET)
         const practitioner = await Practitioner.findOne({ _id: decoded._id, isDeleted: false, 'tokens.token': token })
         
         if (!practitioner) {
